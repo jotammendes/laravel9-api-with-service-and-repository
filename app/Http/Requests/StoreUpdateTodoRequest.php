5fe78cfd4de32dbbc489eac2ee63be9ed6115516
João Paulo Matos Mendes <jpaulo.mmendes@gmail.com>
2022-05-26 22:50:25 -0300

    feature: implementação de TodoResourse e TodoRequest

--- app/Http/Requests/StoreUpdateTodoRequest.php
+++ app/Http/Requests/StoreUpdateTodoRequest.php
@@ -13,7 +13,7 @@ class StoreUpdateTodoRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class StoreUpdateTodoRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => ['required', 'string', 'min:3', 'max:255'],
+            'description' => ['nullable', 'string', 'min:3', 'max:9999'],
+            'completed' => ['required', 'boolean'],
         ];
     }
 }
--- app/Http/Resources/TodoResource.php
+++ app/Http/Resources/TodoResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TodoResource extends JsonResource
@@ -14,6 +15,11 @@ class TodoResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'title' => strtolower($this->name),
+            'body' => $this->description,
+            'completed' => $this->completed ? 'S': 'N',
+            'date' => Carbon::create($this->created_at)->format('d/m/Y'),
+        ];
     }
 }
