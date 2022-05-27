6d6c10d4767e0ac07d44e7dbb65f1e2b5ded6ecb
João Paulo Matos Mendes <jpaulo.mmendes@gmail.com>
2022-05-26 22:51:48 -0300

    feature: criação de rota para Todo

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::apiResource('todos', TodoController::class);
+
 Route::get('/', function () {
     return response()->json([
         'success' => true,
