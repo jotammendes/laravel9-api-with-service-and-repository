3a8b2f6e1564a73bb5375e0e7c87ad234eb7a563
João Paulo Matos Mendes <jpaulo.mmendes@gmail.com>
2022-05-26 22:25:14 -0300

    feature: implementação da parte do TodoRepository referente ao CRUD Todo

--- app/Repositories/TodoRepository.php
+++ app/Repositories/TodoRepository.php
@@ -12,4 +12,31 @@ class TodoRepository
     {
         $this->entity = $todo;
     }
+
+    public function getAll()
+    {
+        return $this->entity->all();
+    }
+
+    public function get(string $identify)
+    {
+        return $this->entity->findOrFail($identify);
+    }
+
+    public function createNew(array $data)
+    {
+        return $this->entity->create($data);
+    }
+
+    public function update(string $identify, array $data)
+    {
+        $todo = $this->get($identify);
+        return $todo->update($data);
+    }
+
+    public function delete(string $identify)
+    {
+        $todo = $this->get($identify);
+        return $todo->delete();
+    }
 }
